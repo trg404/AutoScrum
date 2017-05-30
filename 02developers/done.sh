790efeb11dd39ae21e7b963d51157897640aed85
trg404 <trg404@gmail.com>
2017-05-30 11:02:34 -0500

    added programmers

--- 02developers/done.sh
+++ 02developers/done.sh
@@ -3,3 +3,4 @@
 
 
 ghi list |grep -v trg404| head -1
+[LCAscripts.sh]&&grab number and move to done.
---
+++ 02developers/lca/lcaScripts.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/perl
+
+use cpan::stuff
+
+asfladsf;gkjbadslkadg;kladj;afdlkbagskbjbfdjklaf
